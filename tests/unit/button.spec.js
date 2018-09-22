b7c16a4109f622136608c1c9a3e02c21ae4fb625
Avo <go.go.gg.rostislav@gmail.com>
2018-09-22 10:49:23 +0300

    test corrected

--- tests/unit/button.spec.js
+++ tests/unit/button.spec.js
@@ -2,10 +2,11 @@ import { shallowMount } from '@vue/test-utils'
 import Button from '@/components/Button/Button.vue'
 
 describe('Button.vue', () => {
-  it('Test click event', () => {
-    const mockCallBack = jest.fn()
-    const button = shallowMount((<Button onClick={mockCallBack}>Click</Button>))
-    button.find('button').simulate('click')
-    expect(mockCallBack.mock.calls.length).toEqual(1)
+  it('renders props.text when passed', () => {
+    const text = 'test the button'
+    const wrapper = shallowMount(Button, {
+      propsData: { text }
+    })
+    expect(wrapper.text()).toMatch(text)
   })
 })
