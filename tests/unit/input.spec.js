679c61d20ed4a93c2a11fe7325f8409d6f097054
rsabirov <rsabirov@ozon.ru>
2018-09-25 16:12:25 +0300

    linter

--- tests/unit/input.spec.js
+++ tests/unit/input.spec.js
@@ -2,7 +2,7 @@ import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Input from '@/components/Input/Input.vue'
 const localVue = createLocalVue()
 
-localVue.directive('restrict',  {})
+localVue.directive('restrict', {})
 describe('Input.vue', () => {
   it('Props.value when passed', () => {
     const value = 'Value placeholder'
