621eb3c751bab1a6b16f9986ad25e16339877bd3
ololokiras <bellendirr@yandex.ru>
2018-09-30 15:38:23 +0500

    prepare

---
+++ src/views/UserInfo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <img  class="" />
+    <p></p>
+    <img/>
+  </div>
+</template>
+
+<script type="text/javascript">
+import { mapGetters } from 'vuex'
+export default {
+  name:'Userinfo',
+  computed: {
+    ...mapGetters({
+      userName: 'user/getUserName'
+    }),
+    // stub till without auth
+    getName() {
+      if (!this.userName) return 'default'
+      return this.userName
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .singleline {
+    @apply .inline-block
+  }
+  .user_img {
+    background-image: url(@assets/svg/user_icon.svg);
+    background-size: contain;
+
+  }
+  
+</style>
