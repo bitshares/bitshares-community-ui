1c0cbb7685a8f7976c7a2f8c87cb5849049274e3
Рома <roma219@yandex.ru>
2018-10-07 20:45:12 +0300

    fix test

--- tests/unit/input.spec.js
+++ tests/unit/input.spec.js
@@ -1,26 +1,28 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Input from '@/components/Input/Input.vue'
+import Vuelidate from 'vuelidate'
 const localVue = createLocalVue()
 localVue.directive('restrict', {})
+localVue.use(Vuelidate)
 
 describe('Input.vue', () => {
   it('Props.value set inputs value', () => {
     const value = 'Value placeholder'
-    const wrapper = shallowMount(Input, { propsData: { value }, localVue })
+    const wrapper = shallowMount(Input, { propsData: { value, errors: {}, inputName: 'userName' }, localVue })
 
     expect(wrapper.find('input').element.value).toMatch(value)
   })
 
   it('Props.disabled disables input', () => {
     const disabled = true
-    const wrapper = shallowMount(Input, { propsData: { disabled }, localVue })
+    const wrapper = shallowMount(Input, { propsData: { disabled, errors: {}, inputName: 'userName' }, localVue })
 
     expect(wrapper.find('input').element.disabled).toBe(disabled)
   })
 
   it('Props.title displays title', () => {
     const title = 'test title'
-    const wrapper = shallowMount(Input, { propsData: { title }, localVue })
+    const wrapper = shallowMount(Input, { propsData: { title, errors: {}, inputName: 'userName' }, localVue })
 
     expect(wrapper.find('.input__title').isVisible()).toBe(true)
     expect(wrapper.find('.input__title').html()).toMatch(title)
