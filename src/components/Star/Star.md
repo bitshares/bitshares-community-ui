d0cb954bc73b47d27f6bbe0e291f3fc2c1ed8a59
Rostislav Gogolauri <go.go.gg.rostislav@gmail.com>
2018-10-16 18:02:59 +0300

    star component

---
+++ src/components/Star/Star.md
@@ -0,0 +1,22 @@
+Examples:
+
+Star active by default
+```js
+    <Star :active="true" />
+```
+
+Star inactive by default
+```js
+    <Star :active="true" />
+```
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/Star/Star.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    :class="{
+      'filled': active === true
+    }"
+    class="star">
+    <span>☆</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    active: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.star > span:hover:before {
+   content: "\2605";
+   position: absolute;
+}
+</style>
