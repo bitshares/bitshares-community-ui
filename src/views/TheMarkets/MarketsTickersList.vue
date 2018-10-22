3881b0a66fd940bc8deac91cded35ae4536cb034
lapinlapin <wminvazia@gmail.com>
2018-10-22 14:57:28 +0300

    #104: some fix in column

--- src/views/TheMarkets/MarketsTickersList.vue
+++ src/views/TheMarkets/MarketsTickersList.vue
@@ -75,51 +75,54 @@ export default {
     ._alignRight {
       text-align: right;
     }
-    ._flex05 {
-      flex: .5;
-    }
-  }
-  .tickers-list-head {
-    display: flex;
+    .tickers-list-head {
+      display: flex;
 
-    .tickers-list__field {
-      font-size: 13px;
-      flex: 1;
-      text-transform: none;
-      color: config('colors.tab-header');
-      font-family: config('fonts.gotham');
+      .tickers-list__field {
+        font-size: 13px;
+        flex: 1;
+        text-transform: none;
+        color: config('colors.tab-header');
+        font-family: config('fonts.gotham');
+      }
+      ._flex05 {
+        flex: .6;
+      }
     }
-  }
-  .tickers-list-row {
-    margin-top: 12px;
-    display: flex;
+    .tickers-list-row {
+      margin-top: 12px;
+      display: flex;
 
-    .tickers-list__item {
-      flex: 1;
-      font-size: 13px;
-      text-transform: none;
-      color: config('colors.tab-header');
-      font-family: config('fonts.gotham');
-    }
-    .tickers-list__itemVolume {
-      font-size: 12px;
-      color: config('colors.white');
-      opacity: .8;
-    }
-    ._tickerTitle {
-      font-size: 12px;
-    }
-    ._currencyTitle {
-      color: config('colors.white');
-      font-size: 14px;
-    }
-    ._increase {
-      color: config('colors.increase');
-      opacity: 1;
-    }
-    ._drop {
-      color: config('colors.drop');
-      opacity: 1;
+      .tickers-list__item {
+        flex: 1;
+        font-size: 13px;
+        text-transform: none;
+        color: config('colors.tab-header');
+        font-family: config('fonts.gotham');
+      }
+      .tickers-list__itemVolume {
+        font-size: 12px;
+        color: config('colors.white');
+        opacity: .8;
+      }
+      ._tickerTitle {
+        font-size: 12px;
+      }
+      ._currencyTitle {
+        color: config('colors.white');
+        font-size: 14px;
+      }
+      ._increase {
+        color: config('colors.increase');
+        opacity: 1;
+      }
+      ._drop {
+        color: config('colors.drop');
+        opacity: 1;
+      }
+      ._flex05 {
+        flex: .6;
+      }
     }
   }
 </style>
