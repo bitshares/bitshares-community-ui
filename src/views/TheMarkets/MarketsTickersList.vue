d9f94ea8955c92a44aab20d4b56a604c99dec251
lapinlapin <wminvazia@gmail.com>
2018-10-22 14:40:43 +0300

    #104: made markup of market list

--- src/views/TheMarkets/MarketsTickersList.vue
+++ src/views/TheMarkets/MarketsTickersList.vue
@@ -1,9 +1,45 @@
 <template>
   <div class="tickers-list">
     <div class="tickers-list-head">
-      <div class="tickers-list__item">{{ pairTitle }}</div>
-      <div class="tickers-list__item">{{ priceTitle }}</div>
-      <div class="tickers-list__item">{{ changeTitle }}</div>
+      <div class="tickers-list__field">{{ pairTitle }}</div>
+      <div class="tickers-list__field _flex05">{{ priceTitle }}</div>
+      <div class="tickers-list__field _alignRight">{{ changeTitle }}</div>
+    </div>
+    <!--test row increase-->
+    <div class="tickers-list-row">
+      <div class="tickers-list__item">
+        <div class="tickers-list__itemPair">
+          <span class="_currencyTitle">ZIL</span>
+          <span class="_tickerTitle"> /{{currentTicker}}</span>
+        </div>
+        <div class="tickers-list__itemVolume">10 764 160</div>
+      </div>
+      <div class="tickers-list__item _flex05">
+        <div class="_currencyTitle">0.034810</div>
+        <div class="tickers-list__itemVolume">$0.04</div>
+      </div>
+      <div class="tickers-list__item _alignRight">
+        <div class="_currencyTitle _increase">+37.77%</div>
+        <div class="tickers-list__itemVolume _increase">+12%</div>
+      </div>
+    </div>
+    <!--test row drop-->
+    <div class="tickers-list-row">
+      <div class="tickers-list__item">
+        <div class="tickers-list__itemPair">
+          <span class="_currencyTitle">ZCN</span>
+          <span class="_tickerTitle"> /{{currentTicker}}</span>
+        </div>
+        <div class="tickers-list__itemVolume">934 230</div>
+      </div>
+      <div class="tickers-list__item _flex05">
+        <div class="_currencyTitle">9.29</div>
+        <div class="tickers-list__itemVolume">$0.03</div>
+      </div>
+      <div class="tickers-list__item _alignRight">
+        <div class="_currencyTitle _drop">-0.12%</div>
+        <div class="tickers-list__itemVolume _drop">-17%</div>
+      </div>
     </div>
   </div>
 </template>
@@ -31,11 +67,31 @@ export default {
 </script>
 
 <style lang="scss">
+
   .tickers-list {
     margin-top: 15px;
-  }
+    font-weight: 600;
 
+    ._alignRight {
+      text-align: right;
+    }
+    ._flex05 {
+      flex: .5;
+    }
+  }
   .tickers-list-head {
+    display: flex;
+
+    .tickers-list__field {
+      font-size: 13px;
+      flex: 1;
+      text-transform: none;
+      color: config('colors.tab-header');
+      font-family: config('fonts.gotham');
+    }
+  }
+  .tickers-list-row {
+    margin-top: 12px;
     display: flex;
 
     .tickers-list__item {
@@ -44,6 +100,26 @@ export default {
       text-transform: none;
       color: config('colors.tab-header');
       font-family: config('fonts.gotham');
+    }
+    .tickers-list__itemVolume {
+      font-size: 12px;
+      color: config('colors.white');
+      opacity: .8;
+    }
+    ._tickerTitle {
+      font-size: 12px;
+    }
+    ._currencyTitle {
+      color: config('colors.white');
+      font-size: 14px;
+    }
+    ._increase {
+      color: config('colors.increase');
+      opacity: 1;
+    }
+    ._drop {
+      color: config('colors.drop');
+      opacity: 1;
     }
   }
 </style>
