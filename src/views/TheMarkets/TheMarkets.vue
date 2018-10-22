cc6b51da2ab500bb3bca5048399417a95922beaf
lapinlapin <wminvazia@gmail.com>
2018-10-22 11:25:05 +0300

    #104: add styles for market search input

--- src/views/TheMarkets/TheMarkets.vue
+++ src/views/TheMarkets/TheMarkets.vue
@@ -1,17 +1,49 @@
 <template>
   <div class="markets">
-    <Tabs
-      :tabs="['USD', 'BTC', 'ETH', 'CNY', 'BTS']"
-      :currency-mode="true"
-    />
+    <div class="markets__header">
+      <div class="ticker-sidebar">
+        <Tabs
+          :tabs="['USD', 'BTC', 'ETH', 'CNY', 'BTS']"
+          :currency-mode="true"
+        />
+      </div>
+      <div class="ticker-search">
+        <input type="text" placeholder="Search">
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import Tabs from '@/components/Tabs'
+import SearchInput from '@/components/Input/'
 
 export default {
   components: {
-    Tabs
+    Tabs,
+    SearchInput
+  },
+  data() {
+    return {
+      searchValue: '',
+      searchErrors: {}
+    }
   }
 }
 </script>
+<style lang="scss" scoped>
+  .markets__header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+    .ticker-search {
+      input {
+        border: none;
+        outline: none;
+        background: config('colors.card-background');
+        color: config('colors.white');
+        border-bottom: 1px solid #7a7675;
+      }
+    }
+  }
+</style>
