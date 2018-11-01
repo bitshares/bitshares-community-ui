0ec0e38a38cf395e4b57c450ce1f08e7717593f0
Rostislav Gogolauri 'mindfulme <alice.gogolauri@gmail.com>
2018-11-01 20:39:59 +0200

    unit test with the amount of 100

--- tests/unit/amountValueShortener.spec.js
+++ tests/unit/amountValueShortener.spec.js
@@ -1,6 +1,13 @@
 import { amountValueShortener } from '@/helpers/utils'
 
 describe('amountValueShortener function', () => {
+  it('Test amount shortener used with 100', () => {
+    const amount = '100'
+    const finalAmount = '100'
+    const result = amountValueShortener(amount)
+    expect(result).toBe(finalAmount)
+  })
+      
   it('Test amount shortener with thousands', () => {
     const amount = '12000'
     const finalAmount = '12k'
@@ -14,4 +21,6 @@ describe('amountValueShortener function', () => {
     const result = amountValueShortener(amount)
     expect(result).toBe(finalAmount)
   })
+
+
 })
