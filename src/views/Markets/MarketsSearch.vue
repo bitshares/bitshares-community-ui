87593df3c5f0d1827ada69ea158e43447531dcdc
lapinlapin <wminvazia@gmail.com>
2018-11-06 16:38:07 +0300

    #145: no hide search, when searchStr exist

--- src/views/Markets/MarketsSearch.vue
+++ src/views/Markets/MarketsSearch.vue
@@ -1,17 +1,23 @@
 <template>
   <SearchInput
+    :value="searchStr"
     hint="Search"
     @input="changeSearchStr"
   />
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 import SearchInput from '@/components/SearchInput'
 
 export default {
   components: {
     SearchInput
+  },
+  computed: {
+    ...mapGetters({
+      searchStr: 'marketsMonitor/getSearchStr'
+    })
   },
   methods: {
     ...mapActions('marketsMonitor', ['changeSearchStr'])
