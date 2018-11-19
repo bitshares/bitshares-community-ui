420c11ce83b1fc08773de0c1b916ddea6410b1e1
lapinlapin <wminvazia@gmail.com>
2018-11-19 15:26:17 +0300

    #139: test:actions - setCurrentBase

--- tests/unit/markets/actions.spec.js
+++ tests/unit/markets/actions.spec.js
@@ -1,28 +1,28 @@
 import markets from '@/store/modules/marketsMonitor'
-import { createLocalVue } from 'vue-test-utils';
-import Vuex from 'vuex';
+import { createLocalVue } from 'vue-test-utils'
+import Vuex from 'vuex'
 
-const localVue = createLocalVue();
-localVue.use(Vuex);
+const localVue = createLocalVue()
+localVue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
     markets
   }
-});
+})
 
-const initialState = JSON.parse(JSON.stringify(store.state));
+const initialState = JSON.parse(JSON.stringify(store.state))
 
 describe('markets module: actions', () => {
   beforeEach(() => {
-    store.replaceState(JSON.parse(JSON.stringify(initialState)));
-  });
+    store.replaceState(JSON.parse(JSON.stringify(initialState)))
+  })
 
   it('changeSearchStr', async done => {
-    await store.commit('markets/UPDATE_SEARCH_STR', 'find');
-    expect(store.state.markets.searchStr).toBe('find');
-    done();
-  });
+    await store.commit('markets/UPDATE_SEARCH_STR', 'find')
+    expect(store.state.markets.searchStr).toBe('find')
+    done()
+  })
 
   it('toggleFavourite', async done => {
     const base = 'USD'
@@ -30,10 +30,26 @@ describe('markets module: actions', () => {
     await store.dispatch('markets/toggleFavourite', {
       base,
       quote
-    });
+    })
     expect(store.state.markets.favourites).toEqual({
       USD: ['BTC']
-    });
-    done();
-  });
-});
+    })
+    done()
+  })
+
+  it('setCurrentBase: currentBase = favourites', async done => {
+    const base = 'favourites'
+
+    await store.dispatch('markets/setCurrentBase', base)
+    expect(store.state.markets.currentBase).toBe('favourites')
+    done()
+  })
+
+  it('setCurrentBase: currentBase = other', async done => {
+    const base = 'BTC'
+
+    await store.dispatch('markets/setCurrentBase', base)
+    expect(store.state.markets.currentBase).toBe('BTC')
+    done()
+  })
+})
