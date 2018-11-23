b4a04e0a15adcd6bc4984fc004ef2b4a5437e44d
lapinlapin <wminvazia@gmail.com>
2018-11-23 11:31:13 +0300

    #212: Created component of TwoLineItem

---
+++ src/components/TwoLineItem/TwoLineItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    :class="{'two-line-item--expanded': expanded}"
+    class="two-line-item"
+  >
+    <div class="two-line-item-top">{{ top }}</div>
+    <div class="two-line-item-bottom">{{ bottom }}</div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    top: {
+      type: String,
+      default: ''
+    },
+    bottom: {
+      type: String,
+      default: ''
+    },
+    expanded: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+<style lang="scss">
+  .two-line-item {
+    .two-line-item-top {
+      font-size: config('textSizes.lg');
+      color: config('colors.primary');
+      overflow: hidden;
+      word-break: break-all;
+      padding-top: .3rem;
+    }
+    .two-line-item-bottom {
+      font-size: config('textSizes.sm');
+      color: config('colors.inactive');
+    }
+    &--expanded {
+      .two-line-item-top {
+        font-size: config('textSizes.xl');
+      }
+      .two-line-item-bottom {
+        font-size: config('textSizes.base');
+      }
+    }
+  }
+</style>
