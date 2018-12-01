9586255725a9db6e70aab5725dd3b9c5631691f8
ololokiras <bellendirr@yandex.ru>
2018-12-02 00:09:44 +0500

    parse all available data from active orders

--- src/store/modules/activeOrders.js
+++ src/store/modules/activeOrders.js
@@ -114,6 +114,52 @@ const getters = {
     return state.isFetching
   },
   getActiveList: (state, getters, rootState, rootGetters) => {
+    const orders = rootGetters['acc/getActiveOrders']
+    console.log('active orders: ', orders)
+    const parsedActiveOrders = orders.map(order => {
+      // const dateOpen =
+      const side = order.deferred_paid_fee.asset_id === order.sell_price.asset_id ? 'buy' : 'sell'
+      console.log(side)
+      const payAsset = side === 'buy'
+        ? rootGetters['assets/getAssetById'](order.sell_price.base.asset_id)
+        : rootGetters['assets/getAssetById'](order.sell_price.quote.asset_id)
+      const receiveAsset = side === 'buy'
+        ? rootGetters['assets/getAssetById'](order.sell_price.quote.asset_id)
+        : rootGetters['assets/getAssetById'](order.sell_price.base.asset_id)
+      const payAssetSymbol = payAsset.symbol
+      const receiveAssetSymbol = receiveAsset.symbol
+      console.log('pay asset symbol:', payAssetSymbol)
+      console.log('receiveAssetSymbol:', receiveAssetSymbol)
+      let filled = side === 'sell'
+        ? order.sell_price.base.amount - order.for_sale
+        : order.sell_price.quote.amount - order.for_sale
+      filled = (filled * 100).toFixed()
+      console.log(filled)
+      const precisionDiff = side === 'buy'
+        ? payAsset.precision - receiveAsset.precision
+        : receiveAsset.precision - payAsset.precision
+      const price = side === 'buy'
+        ? ((order.sell_price.base.amount / order.sell_price.quote.amount) * (10 ** precisionDiff)).toFixed(8)
+        : ((order.sell_price.quote.amount / order.sell_price.base.amount) * (10 ** precisionDiff)).toFixed(8)
+      console.log(price)
+      const spend = side === 'buy'
+        ? order.sell_price.quote.amount / (10 ** receiveAsset.precision)
+        : order.sell_price.base.amount / (10 ** receiveAsset.precision)
+      const get = side === 'buy'
+        ? order.sell_price.base.amount / (10 ** payAsset.precision)
+        : order.sell_price.quote.amount / (10 ** payAsset.precision)
+      console.log('spend', spend)
+      console.log('get', get)
+      return {
+        get,
+        spend,
+        price,
+        filled,
+        payAssetSymbol,
+        receiveAssetSymbol
+      }
+    })
+    // return parsedActiveOrders
     // now its demo case for activeOrders test
     return state.activeList.map(listItem => Object.assign(
       {},
