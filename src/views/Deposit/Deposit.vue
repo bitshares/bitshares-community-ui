de7477e2e8b13c6a4c6b6d670172fdda9c146c73
lapinlapin <wminvazia@gmail.com>
2018-12-12 12:27:14 +0300

    #262: add scroll, add searching

--- src/views/Deposit/Deposit.vue
+++ src/views/Deposit/Deposit.vue
@@ -1,18 +1,74 @@
 <template>
   <div class="deposit-container h-full sm:w-120">
-    <div class="title">Deposit</div>
-    <div>Example content</div>
-    <Button
+    <div class="deposit-title">Deposit</div>
+    <div class="deposit-sub-title">Choose token</div>
+    <div class="desposit-search">
+      <SimpleInput
+        :value="searchStr"
+        :centered="true"
+        @input="onSearch"
+      />
+    </div>
+    <ScrollingContainer
+      :shadower-height="0"
+    >
+      <div class="deposit-content">
+        <div
+          v-for="(deposit, index) in filteredList"
+          :key="index"
+          class="deposit-item"
+        >
+          <Button
+            :text="deposit"
+            type="secondary"
+            width="full"
+            class="example-button"
+          />
+        </div>
+      </div>
+    </ScrollingContainer>
+    <!--<Button
       class="example-button"
-      text="example button"/>
+      text="example button"/>-->
   </div>
 </template>
 
 <script>
 import Button from '@/components/Button'
+import ScrollingContainer from '@/components/ScrollingContainer'
+import SimpleInput from '@/components/SimpleInput'
 
 export default {
-  components: { Button }
+  components: {
+    Button,
+    ScrollingContainer,
+    SimpleInput
+  },
+  data() {
+    return {
+      searchStr: '',
+      list: [
+        '32BIT (32bit)',
+        '8BIT (8Bit)',
+        'ANTI (AntiBitcoin)',
+        'bifd (bifd)',
+        'BIGUP (BigUp)',
+        'bill (bill)',
+        'BITB (BitBean)'
+      ]
+    }
+  },
+  computed: {
+    filteredList() {
+      if (!this.searchStr) return this.list
+      return this.list.filter(deposit => deposit.indexOf(this.searchStr) > -1)
+    }
+  },
+  methods: {
+    onSearch(str) {
+      this.searchStr = str
+    }
+  }
 }
 </script>
 <style lang="scss">
@@ -21,12 +77,24 @@ export default {
     padding: 0.6rem;
     display: flex;
     flex-direction: column;
-    min-height: 20rem;
-    .title {
-      width: 100%;
+    height: 100%;
+    overflow: hidden;
+
+    .deposit-content {
+      height: 100%;
+    }
+    .deposit-title {
+      font-size: config('textSizes.lg');
       text-align: center;
       text-transform: uppercase;
       margin-bottom: 1rem;
+    }
+    .deposit-sub-title {
+      font-size: config('textSizes.lg');
+      text-align: center;
+    }
+    .deposit-item {
+      margin-top: 15px;
     }
     .example-button {
       margin-top: auto;
