2b8445029e404ca1559ac94ff6f24cb5a45572e3
lapinlapin <wminvazia@gmail.com>
2018-12-14 11:41:20 +0300

    #284: add initial withdraw

---
+++ src/views/Withdraw/Withdraw.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="withdraw-container h-full sm:w-120">
+    <div class="withdraw-title">withdraw</div>
+    <div class="withdraw-sub-title">Choose token</div>
+  </div>
+</template>
+<script>
+</script>
+<style lang="scss">
+  .withdraw-container {
+    color: config('colors.primary');
+    padding: 0.6rem;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+
+    .withdraw-title {
+      font-size: config('textSizes.lg');
+      text-align: center;
+      text-transform: uppercase;
+      margin-bottom: 1rem;
+    }
+    .withdraw-sub-title {
+      font-size: config('textSizes.lg');
+      text-align: center;
+    }
+  }
+</style>
