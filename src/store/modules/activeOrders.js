9b32876ad436167970d19abb1306d6ae1080802f
ololokiras <bellendirr@yandex.ru>
2018-12-17 12:11:37 +0500

    add parse function to module activeOrders

--- src/store/modules/activeOrders.js
+++ src/store/modules/activeOrders.js
@@ -9,6 +9,52 @@ const state = {
   activeList: []
 }
 
+const parseOpenOrders = (orders, rootGetters) => {
+  const parsedActiveOrders = orders.map(order => {
+    // need to get if from somewhere
+    const isBid = true
+
+    const payInSellPrice = isBid ? order.sell_price.base : order.sell_price.quote
+    const payAssetId = payInSellPrice.asset_id
+    const payAsset = rootGetters['assets/getAssetById'](payAssetId)
+
+    const receiveInSellPrice = isBid ? order.sell_price.quote : order.sell_price.base
+    const receiveAssetId = receiveInSellPrice.asset_id
+    const receiveAsset = rootGetters['assets/getAssetById'](receiveAssetId)
+
+    const get = isBid
+      ? parseFloat(receiveInSellPrice.amount) / (10 ** receiveAsset.precision)
+      : parseFloat(payInSellPrice.amount) / (10 ** payAsset.precision)
+    const spend = isBid
+      ? parseFloat(payInSellPrice.amount) / (10 ** payAsset.precision)
+      : parseFloat(receiveInSellPrice.amount) / (10 ** receiveAsset.precision)
+
+    const payAssetSymbol = isBid ? payAsset.symbol : receiveAsset.symbol
+    const receiveAssetSymbol = isBid ? receiveAsset.symbol : payAsset.symbol
+    const filled = (order.sell_price.base.amount - order.for_sale) / (order.sell_price.base.amount)
+
+    const price = isBid
+      ? parseFloat(spend / get)
+      : parseFloat(get / spend)
+
+    const expiration = (new Date(order.expiration)).getTime()
+
+    return {
+      payAssetSymbol,
+      receiveAssetSymbol,
+      expiration,
+      get,
+      order: isBid ? 'buy' : 'sell',
+      vol: isBid ? get : spend,
+      spend,
+      price,
+      filled,
+      orderId: order.id
+    }
+  })
+  return parsedActiveOrders
+}
+
 const getters = {
   getSearchStr(state) {
     return state.searchStr
@@ -17,7 +63,8 @@ const getters = {
     return state.isFetching
   },
   getActiveList: (state, getters, rootState, rootGetters) => {
-    return rootGetters['acc/getActiveOrders']
+    const dirtyActiveOrders = rootGetters['acc/getActiveOrders']
+    return parseOpenOrders(dirtyActiveOrders, rootGetters)
   }
 }
 
