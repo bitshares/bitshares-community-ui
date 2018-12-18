8d8a1f07f5f05eff1944e6f34b206bf06ad68c06
Keegan Francis <keegan.lee.francis@gmail.com>
2018-12-18 16:42:21 -0400

    #275 Mobile Flash Messages - Implement toastr library instead of notie

--- src/helpers/toast.js
+++ src/helpers/toast.js
@@ -1,18 +1,34 @@
-import { alert } from 'notie'
-import 'notie/dist/notie.min.css'
+import toastr from 'toastr'
+import 'toastr/build/toastr.css'
+
+// Show the close button
+toastr.options.closeButton = true
+
+// Hide / Show method
+toastr.options.showMethod = 'slideDown'
+toastr.options.closeMethod = 'fadeOut'
+
+// Show a progress bar on each toaster, indicating the duration left to remain open
+toastr.options.progressBar = true
+
+// Prevent Duplicate Toasts
+toastr.options.preventDuplicates = true
+
+// Show the toaster in the bottom right hand corner
+toastr.options.positionClass = 'toast-bottom-right'
 
 export default {
   error(text) {
-    this.alert('error', text)
+    toastr.error('error', text)
   },
   warning(text) {
-    this.alert('warning', text)
+    toastr.warning('warning', text)
   },
   success(text) {
-    this.alert('success', text)
+    toastr.success('success', text)
   },
   info(text) {
-    this.alert('info', text)
+    toastr.info('info', text)
   },
   alert(type, text) {
     alert({ type, text, position: 'bottom' })
