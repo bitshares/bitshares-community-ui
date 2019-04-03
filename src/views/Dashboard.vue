6c4b0407813e585457906e81b8b94cfa8cc906cd
lapinlapin <wminvazia@gmail.com>
2019-04-03 14:30:06 +0300

    fix dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,127 +1,127 @@
 <template>
-
-  <v-breakpoint>
-    <div
-      slot-scope="scope"
-      class="dashboard">
-      <Modal
-        v-if="backupFlag"
-        @close="toggleBackupModal"
-      >
-        <Backup/>
-      </Modal>
-
+  <div>
+    <v-breakpoint v-model="scope">
       <div
-        v-if="scope.isLarge || scope.isXlarge"
-        class="dashboard hidden lg:block">
-        <div class="flex flex-col lg:flex-row mb-card-margin">
-          <div class="flex flex-col lg:w-1/3 pr-card-row">
-            <div>
-              <Card
-                :expandable="true"
-                :collapsible="true"
-                title="account"
-                class="mb-card-margin"
-              >
-                <AccountHeader slot="header"/>
-                <AccountHeader
-                  slot="modal-header"
-                  :large="true"/>
-                <Portfolio slot="body"/>
-                <Portfolio
-                  slot="modal"
-                  :expanded="true"/>
-              </Card>
-            </div>
-            <div class="flex flex-col">
-              <Card
-                :expandable="true"
-                :collapsible="true"
-                :long="true"
-                title="markets"
-              >
-                <MarketsSearch slot="header"/>
-                <Markets slot="body"/>
-                <MarketsSearch slot="modal-header"/>
-                <Markets
-                  slot="modal"
-                  :expand-mode="true"/>
-              </Card>
-            </div>
+        class="dashboard">
+        <Modal
+          v-if="backupFlag"
+          @close="toggleBackupModal"
+        >
+          <Backup/>
+        </Modal>
 
-          </div>
-
-          <div class="flex flex-col lg:w-2/3 pr-card-row">
-            <div class="mb-card-margin">
-              <Card
-                :collapsible="true"
-                title="graph"
-              >
-                <Graph slot="body"/>
-              </Card>
-            </div>
-            <div class="flex">
-              <div class="flex flex-col lg:w-1/2 pr-half-card-row">
+        <div
+          v-if="scope.isLarge || scope.isXlarge"
+          class="dashboard hidden lg:block">
+          <div class="flex flex-col lg:flex-row mb-card-margin">
+            <div class="flex flex-col lg:w-1/3 pr-card-row">
+              <div>
                 <Card
+                  :expandable="true"
                   :collapsible="true"
+                  title="account"
                   class="mb-card-margin"
                 >
-                  <NewOrder slot="body"/>
+                  <AccountHeader slot="header"/>
+                  <AccountHeader
+                    slot="modal-header"
+                    :large="true"/>
+                  <Portfolio slot="body"/>
+                  <Portfolio
+                    slot="modal"
+                    :expanded="true"/>
                 </Card>
+              </div>
+              <div class="flex flex-col">
                 <Card
                   :expandable="true"
                   :collapsible="true"
-                  title="my active orders"
+                  :long="true"
+                  title="markets"
                 >
-                  <ActiveOrdersSearch slot="header"/>
-                  <ActiveOrders slot="body"/>
-                  <ActiveOrdersSearch slot="modal-header"/>
-                  <ActiveOrders
+                  <MarketsSearch slot="header"/>
+                  <Markets slot="body"/>
+                  <MarketsSearch slot="modal-header"/>
+                  <Markets
                     slot="modal"
-                    :expand-mode="true"
-                  />
+                    :expand-mode="true"/>
                 </Card>
-
               </div>
-              <div class="flex flex-col lg:w-1/2 pl-half-card-row">
-                <div>
+
+            </div>
+
+            <div class="flex flex-col lg:w-2/3 pr-card-row">
+              <div class="mb-card-margin">
+                <Card
+                  :collapsible="true"
+                  title="graph"
+                >
+                  <Graph slot="body"/>
+                </Card>
+              </div>
+              <div class="flex">
+                <div class="flex flex-col lg:w-1/2 pr-half-card-row">
                   <Card
                     :collapsible="true"
-                    title="order book"
                     class="mb-card-margin"
                   >
-                    <OrderBook slot="body"/>
+                    <NewOrder slot="body"/>
                   </Card>
+                  <Card
+                    :expandable="true"
+                    :collapsible="true"
+                    title="my active orders"
+                  >
+                    <ActiveOrdersSearch slot="header"/>
+                    <ActiveOrders slot="body"/>
+                    <ActiveOrdersSearch slot="modal-header"/>
+                    <ActiveOrders
+                      slot="modal"
+                      :expand-mode="true"
+                    />
+                  </Card>
+
                 </div>
+                <div class="flex flex-col lg:w-1/2 pl-half-card-row">
+                  <div>
+                    <Card
+                      :collapsible="true"
+                      title="order book"
+                      class="mb-card-margin"
+                    >
+                      <OrderBook slot="body"/>
+                    </Card>
+                  </div>
 
-                <Card
-                  :expandable="true"
-                  :collapsible="true"
-                  title="My orders history"
-                >
-                  <OrderHistorySearch slot="modal-header"/>
-                  <OrderHistory slot="body"/>
-                  <OrderHistory
-                    slot="modal"
-                    :expand-mode="true"
-                  />
-                </Card>
+                  <Card
+                    :expandable="true"
+                    :collapsible="true"
+                    title="My orders history"
+                  >
+                    <OrderHistorySearch slot="modal-header"/>
+                    <OrderHistory slot="body"/>
+                    <OrderHistory
+                      slot="modal"
+                      :expand-mode="true"
+                    />
+                  </Card>
 
+                </div>
               </div>
             </div>
+
           </div>
-
         </div>
+
+        <Deposit/>
+        <Withdraw/>
+        <ChangePassword/>
+        <Mobile v-if="scope.isSmall || scope.isMedium || scope.noMatch"/>
+
+        <UnlockAccountPopup/>
       </div>
-
-      <Deposit/>
-      <Withdraw/>
-      <ChangePassword/>
-      <Mobile v-if="scope.isSmall || scope.isMedium || scope.noMatch"/>
-
-      <UnlockAccountPopup/>
-    </div>
-  </v-breakpoint>
+    </v-breakpoint>
+  </div>
 </template>
 
 <script>
@@ -146,7 +146,7 @@ import Withdraw from '@/views/Withdraw/WithdrawWidget'
 import ChangePassword from '@/views/ChangePassword/ChangePasswordWidget'
 import UnlockAccountPopup from '@/views/UnlockAccountPopup/UnlockAccountPopup'
 import Graph from '@/views/Graph/Graph'
-import { VBreakpoint } from 'vue-breakpoint-component'
+import { VBreakpoint, Model } from 'vue-breakpoint-component'
 
 import { mapGetters, mapActions } from 'vuex'
 
@@ -175,6 +175,11 @@ export default {
     Graph,
     VBreakpoint,
     ChangePassword
+  },
+  data() {
+    return {
+      scope: new Model()
+    }
   },
   computed: {
     ...mapGetters({
